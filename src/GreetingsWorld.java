f2212036aeea240bb0b952386997f14892f37efe
Anup Kumar <AnupKumarlive@users.noreply.github.com>
2016-02-11 22:02:18 +0530

    Test for creating a repository locally and pushing the same

---
+++ src/GreetingsWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by lenovo g50 on 2/11/2016.
+ */
+public class GreetingsWorld {
+
+public static void main(String args[]){
+
+    System.out.println("Grretings to the World ");
+}
+}
